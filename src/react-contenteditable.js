08d93bd295d98fc514251f11394a77db89132e0b
Geoff Liu <hi@geoffliu.me>
2016-07-27 12:00:25 -0400

    Normalize HTML before comparison

--- src/react-contenteditable.js
+++ src/react-contenteditable.js
@@ -20,12 +20,12 @@ export default class ContentEditable extends React.Component {
   }
 
   shouldComponentUpdate(nextProps) {
-    return !this.htmlEl || nextProps.html !== this.htmlEl.innerHTML ||
+    return !this.htmlEl || this.normalizeHtml(nextProps.html) !== this.htmlEl.innerHTML ||
             this.props.disabled !== nextProps.disabled;
   }
 
   componentDidUpdate() {
-    if ( this.htmlEl && this.props.html !== this.htmlEl.innerHTML ) {
+    if ( this.htmlEl && this.normalizeHtml(this.props.html) !== this.htmlEl.innerHTML ) {
      this.htmlEl.innerHTML = this.props.html;
     }
   }
@@ -38,5 +38,18 @@ export default class ContentEditable extends React.Component {
       this.props.onChange(evt);
     }
     this.lastHtml = html;
+  }
+
+  normalizeHtml(htmlValue) {
+    // Skip browser reformatting when rendering on the server.
+    if (typeof window === 'undefined') {
+      return htmlValue;
+    }
+
+    // Run the HTML string through the browser's reformatting, otherwise it
+    // could throw off our comparison.
+    const tempContainer = document.createElement('div')
+    tempContainer.innerHTML = htmlValue
+    return tempContainer.innerHTML
   }
 }
