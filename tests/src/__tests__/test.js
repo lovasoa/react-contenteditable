e91d9a3ce339e76502176a74692fa9de9371f3b0
ophir <pere.jobs@gmail.com>
2018-08-05 12:27:10 +0200

    Clean up the tests

--- tests/src/__tests__/test.js
+++ tests/src/__tests__/test.js
@@ -2,76 +2,74 @@ const puppeteer = require('puppeteer');
 
 const testFile = 'file://' + __dirname + '/../../build/index.html';
 
-async function getHtml(page) {
-  return page.evaluate(() => editComponent.getHtml());
-}
-
 async function initialState(page) {
   const editableHtml = await page.$eval("#editableDiv", e => e.innerHTML);
   expect(editableHtml).toBe("");
-  const rawHtml = await page.$eval("#editableDiv", e => e.innerHTML);
-  expect(rawHtml).toBe("");
 }
 
-async function textTyped(page) {
+function expectHtml(editComponent, html) {
+  return expect(editComponent('getHtml()')).resolves.toBe(html);
+}
+
+async function textTyped(page, editComponent) {
   await page.type("#editableDiv", "Hello");
-  expect(await getHtml(page)).toBe("Hello");
+  await expectHtml(editComponent, "Hello");
 
   await page.type("#editableDiv", ", World!");
-  expect(await getHtml(page)).toBe("Hello, World!");
+  await expectHtml(editComponent, "Hello, World!");
 }
 
-async function deleteRewrite(page) {
+async function deleteRewrite(page, editComponent) {
   // See: https://github.com/lovasoa/react-contenteditable/issues/91
 
   // type "a"
   await page.type("#editableDiv", "a");
-  expect(await getHtml(page)).toBe("a");
+  await expectHtml(editComponent, "a");
 
-  // reset
+  // reset the contents programmatically
   await page.evaluate(() => editComponent.setHtml(""));
-  expect(await getHtml(page)).toBe("");
+  await expectHtml(editComponent, "");
 
   // Re-type "a"
   await page.type("#editableDiv", "a");
-  expect(await getHtml(page)).toBe("a");
+  await expectHtml(editComponent, "a");
 }
 
-async function resetStyle(page) {
+async function resetStyle(page, editComponent) {
   // See: https://github.com/lovasoa/react-contenteditable/issues/81
 
   // set style
-  await page.evaluate(() => editComponent.setProps({ style: { height: '300px' } }));
+  await editComponent("setProps({ style: { height: '300px' } })");
 
   // type "a"
   await page.type("#editableDiv", "a");
-  expect(await getHtml(page)).toBe("a");
+  await expectHtml(editComponent, "a");
 
   // set the style to the same value again (shouldn't cause a caret jump)
-  await page.evaluate(() => editComponent.setProps({ style: { height: '300px' } }));
+  await editComponent("setProps({ style: { height: '300px' } })");
 
   // type "b"
   await page.type("#editableDiv", "b");
-  expect(await getHtml(page)).toBe("ab");
+  await expectHtml(editComponent, "ab");
 }
 
 async function initialOnChange(page, editComponent) {
   // See: https://github.com/lovasoa/react-contenteditable/issues/42
 
   // Initially, the onChange handler should not have been called a single time
-  expect(await editComponent('history.length')).toBe(0);
+  await expect(editComponent('history.length')).resolves.toBe(0);
 
   // Focus, the content-editable div
   await page.focus("#editableDiv");
 
   // The focus should not have triggered a change event
-  expect(await editComponent('history.length')).toBe(0);
+  await expect(editComponent('history.length')).resolves.toBe(0);
 
   // Blur the editable contents by pressing Tab
   await page.keyboard.press("Tab");
 
   // The blur should not have triggered a change event
-  expect(await editComponent('history.length')).toBe(0);
+  await expect(editComponent('history.length')).resolves.toBe(0);
 }
 
 const testFuns = [
